8f2c6fe6f1e5c43eee4b8931c2270e94ae54bfb5
KasparIsSo <kasparso421@gmail.com>
2018-04-06 01:40:42 -0400

    Going ws route

--- startserver.js
+++ startserver.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
 
+var WebSocket = require('ws');
+
+var wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
+
 var serialserver = require('./p5.serialserver');
 serialserver.start();
 console.log("p5.serialserver is running");
